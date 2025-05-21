e796057d813121ee26d722445a9c335eb291b4af
KanokoTsuchimura <205731749+ran401@users.noreply.github.com>
2025-05-21 14:16:06 +0900

    認可機能の実装完了

--- src/main/java/com/example/demo2/AdminPageController.java
+++ src/main/java/com/example/demo2/AdminPageController.java
@@ -1,11 +1,12 @@
 package com.example.demo2;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminPageController {
-
+	@PreAuthorize("hasRole('ROLE_ADMIN')") // 追記 ROLE_ADMINのユーザのみアクセスを許可
 	@GetMapping("/adminPage")
 	public String adminPage() {
 		return "adminPage";
--- src/main/java/com/example/demo2/UserPageController.java
+++ src/main/java/com/example/demo2/UserPageController.java
@@ -1,10 +1,12 @@
 package com.example.demo2;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserPageController {
+	@PreAuthorize("hasRole('ROLE_USER')") // 追記 ROLE_USERのユーザのみアクセスを許可
 	@GetMapping("/userPage")
 	public String userPage() {
 		return "userPage";
